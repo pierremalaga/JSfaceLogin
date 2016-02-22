febfc2c882ab1049ac2c6d5d01921e3f6f2c0718
baruca94 <vcappanera@gmail.com>
2016-02-22 17:26:15 +0100

    delete diseño

---
+++ Instagram/Diseno/style/mainPage.php
@@ -0,0 +1,82 @@
+<html DOCTYPE!>
+<head>
+    <link rel="stylesheet" href="mainStyle.css"/>
+    <link rel="stylesheet" href="http://weloveiconfonts.com/api/?family=zocial"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf8_bin">
+</head>
+<body>
+    <header>
+        <div class="supHeader">
+            <h1 class="title">JoinSocial</h1>
+        </div>
+    </header>
+   
+
+    <form>
+      <div class="menu">            
+            <label for="fullname">Buscar</label>
+            <input type="text" id="Buscar" />
+            <p><input type="checkbox" value="Perfil" id="Perfil" /> <label for="Perfil">Perfil</label></p>    
+            <p>Redes Sociales</p>
+            <p><input type="checkbox" value="Facebook" id="Facebook" /> <label for="Facebook">Facebook</label></p>
+            <p><input type="checkbox" value="Instagram" id="Instagram" /> <label for="Instagram">Instagram</label></p>
+            <p><input type="checkbox" value="Youtube" id="Youtube" /> <label for="Youtube">Youtube</label></p>
+            <p><input type="checkbox" value="Twitter" id="Twitter" /> <label for="Twitter">Twitter</label></p>
+            <p>Filtrar por </p>
+            <p><input type="checkbox" value="Likes" id="Likes" /> <label for="Likes">Likes</label></p>
+            <p><input type="checkbox" value="Comentarios" id="Comentarios" /> <label for="Comentarios">Comentarios</label></p>
+            <p><input type="checkbox" value="Reproducciones" id="Reproducciones" /> <label for="Reproducciones">Reproducciones</label></p>
+  
+    </form>
+    </div>
+<!--hacer un div global-->
+    <div class = "homepage">
+        <div class="content">APITÁN AMÉRICA CIVIL WAR - TRAILER EXTENDIDO
+            <section class="socialBox">
+                <div class="publicationCard">
+                    <div class="firstRow">
+                        <div class="userInfo">
+                            <p class="uname">Pierre Málaga</p>
+                            <span class="subInfo">publicado a las 11:27</span>
+                        </div>
+                        <div class="publiOptions">
+                            <div class="brandico-facebook-rect socialLogo facebook"></div>
+                            <div class="entypo-down-open moreOptions"></div>
+                        </div>
+                    </div>
+                    <div class="contentPublished">
+                        <div class="image"></div>
+                        <div class="text"><p>Image is a blank image</p></div>
+                    </div>
+                    <div class="socialOptions">
+                        <div class="comments"><span class="subInfo">comentarios</span></div>
+                        <div class="fontawesome-thumbs-up likes"></div>
+                    </div>
+                </div>
+            </section>
+            <section class="socialBox">
+                <div class="publicationCard">
+                    <div class="firstRow">
+                        <div class="userInfo">
+                            <p class="uname">Victor Cappanera</p>
+                            <span class="subInfo">publicado a las 11:27</span>
+                        </div>
+                        <div class="publiOptions">
+                            <div class="brandico-instagram-filled socialLogo insta"></div>
+                            <div class="entypo-down-open moreOptions"></div>
+                        </div>
+                    </div>
+                    <div class="contentPublished">
+                        <div class="image"></div>
+                        <div class="text"><p>Image is a blank image</p></div>
+                    </div>
+                    <div class="socialOptions"></div>
+                </div>
+            </section>
+        </div>
+    </div>
+    <footer>
+
+    </footer>
+</body>
+</html>
